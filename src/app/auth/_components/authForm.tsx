48566c898e9899aef6ec6157287b9d5ec488ef2f
riltonbispo <riltondev@gmail.com>
2025-05-16 18:31:42 -0300

    feat: login with chrome

--- src/app/auth/_components/authForm.tsx
+++ src/app/auth/_components/authForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
@@ -20,9 +20,7 @@ const AuthForm = () => {
   })
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    await signIn("email", {
-      email: values.email
-    })
+    console.log(values)
   }
 
   return (
@@ -32,10 +30,29 @@ const AuthForm = () => {
           <CardTitle className="text-2xl font-bold tracking-tight">
             Sign in to your account
           </CardTitle>
+          <CardDescription>
+            Login with your Apple or Google account
+          </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <Button variant="outline" className="w-full" onClick={() => signIn("google", { callbackUrl: '/'})}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    fill="currentColor"
+                  />
+                </svg>
+                Login with Google
+              </Button>
+              
+              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+
               <FormField
                 control={form.control}
                 name="email"
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/services/database"
-import Nodemailer from "next-auth/providers/nodemailer" 
+import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -9,9 +9,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    Nodemailer({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
+    Google
   ]
 })
