99d1732a2491e1feda086df2fb7931b5b7594e63
riltonbispo <riltondev@gmail.com>
2025-05-18 21:23:59 -0300

    feat(api): post route table

---
+++ src/app/api/table/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/services/database'
+import { Column, Row } from '@/types/table'
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { name, userId, columns, rows } = body
+
+    const newTable = await prisma.table.create({
+      data: {
+        name,
+        userId,
+        columns: {
+          create: columns.map((col: Column) => ({
+            key: col.key,
+            label: col.label,
+          })),
+        },
+        rows: {
+          create: (rows as Row[]).map((row) => ({
+            data: row.data,
+          })),
+        },
+      },
+      include: {
+        columns: true,
+        rows: true,
+      },
+    })
+
+    return NextResponse.json(newTable, { status: 201 })
+  } catch (error) {
+    console.error('[POST /api/tables] Erro:', error)
+    return NextResponse.json({ error: 'Erro ao criar tabela' }, { status: 500 })
+  }
+}
