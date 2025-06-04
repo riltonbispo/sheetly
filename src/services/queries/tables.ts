f74590ab6e70e38825c4564abed487f7fb83f255
riltonbispo <riltondev@gmail.com>
2025-06-04 17:27:36 -0300

    feat: create all routes for tables

--- src/services/queries/tables.ts
+++ src/services/queries/tables.ts
@@ -1,4 +1,4 @@
-import { Table } from '@/types/table'
+import { Column, Row, Table } from '@/types/table'
 import axios from 'axios'
 const baseURL = 'http://localhost:3000'
 
@@ -9,5 +9,29 @@ const req = axios.create({
 export const getTables = async (): Promise<Table[]> => {
   const response = await req.get('api/tables')
 
+  return response.data
+}
+
+export const getTableById = async (id: string): Promise<Table> => {
+  const response = await req.get(`/api/tables/${id}`)
+  return response.data
+}
+
+export const deleteTable = async (id: string): Promise<void> => {
+  await req.delete(`/api/tables/${id}`)
+}
+
+export const updateTable = async (id: string, data: { name: string }): Promise<Table> => {
+  const response = await req.patch(`/api/tables/${id}`, data)
+  return response.data
+}
+
+export const createTable = async (data: {
+  name: string
+  userId: string
+  columns: Column[]
+  rows: Row[]
+}): Promise<Table> => {
+  const response = await req.post('/api/tables', data)
   return response.data
 }
